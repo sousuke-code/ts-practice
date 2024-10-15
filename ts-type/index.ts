11810d29ce581aef445ca1de4d29be9ac70ee3b8
sousuke-code <msousuke1@gmail.com>
2024-10-15 22:03:19 +0900

    型の学習

--- ts-type/index.ts
+++ ts-type/index.ts
@@ -4,3 +4,27 @@ let float: number = 3.14;
 let navigate: number = -0.12;
 let single: string = 'hello';
 let back: string = 'hello';
+
+const person = {
+    name : {
+        first : 'Jack',
+        last: 'Smith',
+    },
+    age : 21
+}
+
+const fruits: string[] = ['Apple', 'Banana', 'Grape']
+
+const book : [string, number, boolean] = ['business', 1500, false];
+
+
+enum CoffeSize  {
+    SHORT,
+    TALL,
+    GRANDE,
+    VENTI
+}
+const coffee = {
+    hot : true,
+    size: CoffeSize.TALL
+}
